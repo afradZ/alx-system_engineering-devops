be4e354348a4ba73148049e3f3761b98c77339ec
afradZ <afradzakaria@gmail.com>
2025-02-27 09:53:28 -0800

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,12 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'install_php':
+  command => '/usr/bin/apt update && /usr/bin/apt install php libapache2-mod-php -y',
+  path    => ['/usr/bin', '/usr/sbin'],
+  onlyif  => 'test ! -f /usr/bin/php',
 }
+
+exec { 'restart_apache':
+  command => '/bin/systemctl restart apache2',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  subscribe => Exec['install_php'],
+}
+
